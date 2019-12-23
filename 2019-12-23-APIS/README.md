5420e2556686cca55ff9b3971d7c91252dd61994
ninjapretzel <ninjapretzel@gmail.com>
2019-12-23 15:53:01 -0700

    Adds API example using Accuweather API

---
+++ 2019-12-23-APIS/README.md
@@ -0,0 +1,9 @@
+## API Keys:
+### Accuweather:
+- Create an account at `https://developer.accuweather.com/`  
+- Create an application at `https://developer.accuweather.com/user/me/apps`
+- Once the app is created, click the app name, and select the keys tab
+- Copy the key into a file called `keys.js` that looks as follows:
+```js
+const accuweatherKey = "{{INSERT YOUR KEY HERE}}";
+```
---
+++ 2019-12-23-APIS/weather.js
@@ -0,0 +1,49 @@
+
+
+$(document).ready(function() { 
+	$.get( `http://dataservice.accuweather.com/locations/v1/topcities/50?apikey=${accuweatherKey}`, 
+	// $.get( `https://dataservice.accuweather.com/locations/v1/regions?apikey=${accuweatherKey}`, 
+	function(res) {
+		console.log(res);
+		console.log(typeof(res))
+		
+		
+		if (res.length) {
+			for (let i = 0; i < res.length; i++) {
+				
+				let element = $("<div></div>");
+				let location = res[i];
+				element.attr("id", location.Key);
+				
+				element.text(`City: ${location.EnglishName}, ${location.Country.EnglishName}`)
+				//element.text("City: " + location.EnglishName + ` Id: ` + location.ID)
+				element.click(function() {
+					$.get("http://dataservice.accuweather.com/forecasts/v1/daily/1day/" + location.Key + `?apikey=${accuweatherKey}`,
+					function(res) {
+						let forecast = res.DailyForecasts[0];
+						let mintemp = forecast.Temperature.Minimum
+						let maxtemp = forecast.Temperature.Maximum
+						console.log("forecast: " +forecast);
+						let felem = $(`#${location.Key} .forecast`);
+						console.log(felem);
+						if (felem.length == 0) {
+							
+							felem = $(`<div></div>`)
+							felem.addClass("forecast");
+							$(`#${location.Key}`).append(felem);
+						}
+							
+						felem.text(`Temperature ${mintemp.Value} ${mintemp.Unit} - ${maxtemp.Value} ${maxtemp.Unit}`)
+						console.log(felem);
+						
+					})
+					console.log(`${location.EnglishName} clicked!`)
+				})
+				
+				$("body").append(element);
+			}
+			
+		}
+		
+	})
+})
