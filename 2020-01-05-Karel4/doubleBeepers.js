0b2520a2cc25edbbb8fa2b19fd31a6baa8d17fa7
ninjapretzel <ninjapretzel@gmail.com>
2020-01-05 14:32:33 -0700

    Adds doubleBeepers.js

---
+++ 2020-01-05-Karel4/doubleBeepers.js
@@ -0,0 +1,34 @@
+function turnRight() { turnLeft(); turnLeft(); turnLeft(); }
+function turnAround() { turnLeft(); turnLeft(); }
+
+function backUp() {
+	turnAround();
+	step();
+	turnAround();
+}
+
+function doubleBeepers() {
+	while (isNearBeeper()) {
+		takeBeeper();
+		step();
+		placeBeeper();
+		backUp();
+	}
+	step();
+
+	while (isNearBeeper()) {
+		takeBeeper();
+		backUp();
+		placeBeeper();
+		placeBeeper();
+		step();
+	}
+
+}
+
+while (isNearBeeper()) {
+	
+	step();
+	doubleBeepers();
+
+}
