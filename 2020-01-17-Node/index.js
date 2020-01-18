d549ef6152acdb9e5635919d6701a96c577e4e00
ninjapretzel <ninjapretzel@gmail.com>
2020-01-18 15:31:46 -0700

    Adds more comments

--- 2020-01-17-Node/index.js
+++ 2020-01-17-Node/index.js
@@ -12,14 +12,18 @@ const app = express()
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// function onRequest(req, res, next) {
-// 	if (req.body && typeof(req.body) === "string") {
-// 		req.body = JSON.parse(req.body);	
-// 	}
-// 	next();
-// }
+// 'middleware' like `bodyParser` sits between express.js and our code 
+// It does stuff to process the request before we receive it 
+//	function onRequest(req, res, next) {
+//		if (req.body && typeof(req.body) === "string") {
+//			req.body = JSON.parse(req.body);	
+//		}
+//		next();
+//	}
 
+// Set up the count to 0 by default
 var count = 0;
+
 // We also load any stuff our app needs, for example, we load the saved count from disk.
 fs.readFile("data.json", (err, data) => {
 	if (err) {
@@ -28,6 +32,7 @@ fs.readFile("data.json", (err, data) => {
 	}
 	console.log("loaded data.json: " + data);
 	var obj = JSON.parse(data)
+	
 	// Once loaded, we assign that global variable to hold the count
 	count = obj.count;
 })
@@ -78,15 +83,19 @@ app.post("/count", (req, res) => {
 	res.send(str + "\n\n");
 })
 
+// We can also set up things to happen on a deeper level.
+// `SIGINT` is an interruption signal from the OS, normally this just terminates the process immediately.
+// We can assign a handler to exit cleanly when that signal is received.
 process.on('SIGINT', () => process.exit())
+// And when we exit cleanly, we can save the current count to a file to load the next time we start.
 process.on('exit', () => {
 	console.log("actually exiting now");
-	
 	var data = {
 		count
 	}
 	fs.writeFileSync("data.json", JSON.stringify(data))
 });
 
+// Finally, we tell the app to start listening on some port:
 const port = 3000
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
