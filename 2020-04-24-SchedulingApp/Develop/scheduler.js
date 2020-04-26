033d952aefc3b0e134f42c31ade1a46c10f15dc7
ninjapretzel <ninjapretzel@yahoo.com>
2020-04-26 15:08:22 -0600

    Adds analogy comments

--- 2020-04-24-SchedulingApp/Develop/scheduler.js
+++ 2020-04-24-SchedulingApp/Develop/scheduler.js
@@ -3,6 +3,12 @@ const NUM_HOURS = 8;
 
 // Dynamically create elements on the page 
 // for the user to edit their schedule with
+// Like if we had a template to build a chistmas tree (analogy)
+// where we would take plywood, paint it, wrap it with lights, etc
+// this function would return the "tree" that we built
+// we can create multiple "nearly identical" copies of the same tree
+// with this "blueprint"/function
+// (they differ only by a number)
 function createHourDiv(hour) {
 	// Start with a div
 	let div = $("<div>");
@@ -35,6 +41,9 @@ function createHourDiv(hour) {
 }
 
 // Update the highlights of all 'hours' 
+// (Analogy): Quickly toggling/flashing the lights on all the trees
+// This is like some rigging a stagehand would do 
+// to prepare some special effect. 
 function updateHighlights() {
 	// Get the element that all hours get added to
 	let hours = $("#hours");
@@ -59,7 +68,6 @@ function updateHighlights() {
 		// Reset it by removing any 
 		// colorization classes that are already on it 
 		child.removeClass("past present future");
-		///child.addClass("past present future");
 		
 		// Compare times to see if it is in past/present/future
 		// and color accordingly.
@@ -75,6 +83,9 @@ function updateHighlights() {
 
 // When page is loaded....
 $(document).ready(function() {
+	
+	// With our analogy, this part would be like setting up
+	// the chistmas trees on the stage, and hooking to the lighting system
 	
 	// Loop to create 
 	for (let i = 0; i < NUM_HOURS+1; i++) {
@@ -99,16 +110,30 @@ $(document).ready(function() {
 	}
 	
 	// Immediately update highlights 
+	// (Analogy): This would be like turning 
+	// the tree lights on for the first time.
 	updateHighlights();
 	
+	// (Analogy):
+	// This would be like telling a stage hand to 
+	// toggle the tree lights on and off quickly every second.
 	// Repeatedly call updateHighlights
 	// timeout is in milliseconds, 1000ms = 1 second
 	setInterval(updateHighlights, 1000);
 	
+	// (Analogy): This would be like having stage hands
+	// update the banner over the stage every day
 	// Set current day with momentjs
 	$("#currentDay").text(moment().format("YYYY MMM, ddd Do"))
 	
 	// Callback for clicking save button
+	// (Analogy): This is like allowing the audience
+	// to have a corkgun to shoot the trees with
+	// and when a tree is shot, it lights up (or something)
+	// (say presents fall out of the specific tree that was shot)
+	// (because we don't really have a way to 'save' data in a play)
+	// (the important thing is just *something* happens
+	// 		to the tree that was shot)
 	$(".saveBtn").on("click", function(event){
 		// Get the id of the clicked button
 		let id = event.currentTarget.id; // "9am"
