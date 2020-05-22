685efee690a63ed1225165af00de905836ba56d8
ninjapretzel <ninjapretzel@yahoo.com>
2020-05-22 15:25:02 -0600

    Writes out readme from template

--- 2020-05-22-readmegenerator/index.js
+++ 2020-05-22-readmegenerator/index.js
@@ -61,25 +61,66 @@ async function main() {
 		},
 	])
 	
-	// // Preview data the user entered if desired
-	// // console.log(data);
-	// // Derive filename to write to 
-	// const filename = data.name.toLowerCase().replace("\s", "") + ".json";
 	
-	// // Try to do some code that may have a problem
-	// try {
-	// 	// Wait for the file to be finished writing
-	// 	result = await fs.writeFile(filename, JSON.stringify(data, null, "\t"));
-	// 	// If there was a failure on the line above,
-	// 	// all lines below will be skipped
-	// 	console.log("Success!");
-	// } catch (err) {
-	// 	// If anything goes wrong, we get some data about what went wrong (`err`)
-	// 	// and control jumps into this `catch` block
-	// 	// and the error is considered handled when the block exits
-	// 	console.log("Could not write file " + filename);
-	// 	console.log(err);	
-	// }
+	let text = `# ${data.projectName}
+
+## Description
+
+${data.desc}
+
+## Table of Contents
+
+* [Installation](#installation)
+
+* [Usage](#usage)
+
+* [License](#license)
+
+* [Contributing](#contributing)
+
+* [Tests](#tests)
+
+* [Questions](#questions)
+
+## Installation
+
+To install necessary dependencies run the following command:
+
+\`\`\`
+${data.dependencies}
+\`\`\`
+
+## Usage
+
+${data.infoUse}
+
+## License
+
+
+
+## Contributing
+
+${data.infoContriubte}
+
+## Tests
+
+To run tests, run the following command:
+
+\`\`\`
+${data.tests}
+\`\`\`
+
+## Questions
+
+If you have any questions about the repository, 
+open an issue or contact [${data.username}](undefined) directly at ${data.email}.
+
+`;
+	
+	await fs.writeFile("README.md", text);
+	
+	console.log("Wrote file README.md!");
+	
 }
 
 // Finally, we call the async method to actually kick off the program.
