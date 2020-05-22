0df889f9bafb72d7be30b73981c4a1a3d20904db
ninjapretzel <ninjapretzel@yahoo.com>
2020-05-22 14:15:06 -0600

    "initial_commit"

---
+++ 2020-05-22-readmegenerator/index.js
@@ -0,0 +1,10 @@
+const inquirer = require('inquirer');
+const fs = require('fs-promise-native');
+
+/// Need to wrap code in an async function in order to use 'await'
+async function main() {
+	
+}
+
+// Finally, we call the async method to actually kick off the program.
+main();
