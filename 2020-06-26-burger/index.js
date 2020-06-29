7f1ea20ef6b27dc142aa8b38d08f96db8de47131
ninjapretzel <ninjapretzel@yahoo.com>
2020-06-29 15:03:58 -0600

    Hooks up burger router

--- 2020-06-26-burger/index.js
+++ 2020-06-26-burger/index.js
@@ -1,12 +1,15 @@
 const mysql = require("mysql2/promise");
 const express = require("express");
 const handlebars = require("express-handlebars");
+const burgersController = require("./controllers/burgers_controller");
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+
+app.use("/burgers", burgersController);
 
 
 
