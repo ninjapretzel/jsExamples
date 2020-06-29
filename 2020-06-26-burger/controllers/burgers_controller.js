0100af6e7b96d5fb7cbe5ffeb500a8076cb386bf
ninjapretzel <ninjapretzel@yahoo.com>
2020-06-29 15:11:55 -0600

    finishes burger controller

--- 2020-06-26-burger/controllers/burgers_controller.js
+++ 2020-06-26-burger/controllers/burgers_controller.js
@@ -5,7 +5,7 @@ const BurgerModel = require("../models/burger");
 const router = express.Router();
 
 router.get("/", async function(request, response) {
-	const burgers = BurgerModel.getAll();
+	const burgers = await BurgerModel.getAll();
 	
 	response.send(burgers);
 });
@@ -14,13 +14,25 @@ router.post("/create", async function(request, response) {
 	const data = request.body;
 	const burger = new Burger(-1, data.name, false);
 	
-	BurgerModel.insert(burger);
+	const id = await BurgerModel.insert(burger);
+	
+	if (id > 0) {
+		response.send({ success: true, id });	
+	} else {
+		response.send({ success: false, errorCode: id });	
+	}
 	
 });
 
 router.post("/devour/:id", async function(request, response){
 	
-	BurgerModel.devour(request.params.id);
+	const errorCode = await BurgerModel.devour(request.params.id);
+	
+	if (!errorCode) {
+		response.send( { success: true });
+	} else {
+		response.send( {success: false, errorCode });
+	}
 	
 });
 
