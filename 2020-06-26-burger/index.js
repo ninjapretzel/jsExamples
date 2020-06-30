43e0f435f385cca9eaaacb01324d2ce8ec499877
ninjapretzel <ninjapretzel@yahoo.com>
2020-06-30 13:56:04 -0600

    Adds burger data to template and renders  data

--- 2020-06-26-burger/index.js
+++ 2020-06-26-burger/index.js
@@ -2,6 +2,7 @@ const mysql = require("mysql2/promise");
 const express = require("express");
 const handlebars = require("express-handlebars");
 const burgersController = require("./controllers/burgers_controller");
+const burgerModel = require("./models/burger");
 
 const app = express();
 const PORT = 3000;
@@ -12,8 +13,12 @@ app.use(express.json());
 app.engine("handlebars", handlebars( {defaultLayout: "main"} ) );
 app.set("view engine", "handlebars");
 
-app.get("/", (request, response) => {
-	response.render('index');
+app.get("/", async (request, response) => {
+	const burgers = await burgerModel.getAll();
+	console.log(burgers);
+	const devoured = burgers.filter(it => it.devoured);
+	const undevoured = burgers.filter(it => !it.devoured);
+	response.render('index', { burgers, devoured, undevoured } );
 });
 
 app.use("/public", express.static(__dirname + "/public"));
