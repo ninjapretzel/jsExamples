fed53f1467bd814a352d9f65ceb20c0ac7010e8c
ninjapretzel <ninjapretzel@yahoo.com>
2020-06-30 13:56:32 -0600

    Hooks up devour buttons

--- 2020-06-26-burger/public/assets/js/index.js
+++ 2020-06-26-burger/public/assets/js/index.js
@@ -10,4 +10,17 @@ $(document).ready(()=>{
 			window.location.reload(true);
 		});
 	});
+	
+	$(".devour").click((event)=>{
+		const burgerID = $(event.target).attr("data");
+		
+		$.post("/burgers/devour/"+burgerID, {}, (data, status) => {
+			// Page will be re-rendered server side.
+			// We tell the browser to discard 
+			// the cached page by passing in "true".
+			window.location.reload(true);
+		});
+	});
+	
+	
 });
