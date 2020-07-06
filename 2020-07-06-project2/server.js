1cc106801f79b732a4a94d09c44f1da3e5c437f4
ninjapretzel <ninjapretzel@yahoo.com>
2020-07-06 15:10:39 -0600

    Added handlebars templates

--- 2020-07-06-project2/server.js
+++ 2020-07-06-project2/server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const session = require("express-session");
+const handlebars = require("express-handlebars");
+
 const app = express();
 const PORT = 3000;
 
@@ -7,6 +9,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
+app.engine("handlebars", handlebars( {defaultLayout: "main"} ) );
+app.set("view engine", "handlebars");
+
+app.get("/", async (request, response) => {
+	response.render('index', { timestamp: new Date() });
+});
 
 async function main() {
 	// await db.sequelize.sync();
