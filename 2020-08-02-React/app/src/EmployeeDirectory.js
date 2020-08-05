b9f938c408e429028bdda4ace7fe2ea7079e8679
ninjapretzel <ninjapretzel@yahoo.com>
2020-08-05 12:19:45 -0600

    adds display of data from api

--- 2020-08-02-React/app/src/EmployeeDirectory.js
+++ 2020-08-02-React/app/src/EmployeeDirectory.js
@@ -2,11 +2,58 @@ import React from 'react';
 import EmployeeLine from './EmployeeLine'
 
 class EmployeeDirectory extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			error: null,
+			isLoaded: false,
+			items: []
+		};
+	}
+	
+	async componentDidMount() {
+		try {
+			let res = await fetch("https://randomuser.me/api/?results=50")
+			let result = await res.json();
+			let employees = result.results.map(it => {
+				return {
+					name: it.name.first + " " + it.name.last,
+					email: it.email,
+					dob: it.dob.date,
+					cell: it.cell,
+					picture: it.picture.thumbnail,
+				}
+			});
+			
+			
+			this.setState({
+				isLoaded: true,
+				items: employees
+			});
+			
+		} catch (error) {
+			this.setState({
+				isLoaded: true,
+				error,
+			})
+		}
+	}
+	
+	
 	render() {
+		const { error, isLoaded, items } = this.state;
+		
+		if (error) {
+			return <div>Error: {error.message}</div>;
+		} else if (!isLoaded) {
+			return <div>Loading...</div>
+		}
+		
 		return <div className="row">
 			<div className="col s12 center">
 				<input id="searchField"></input>
 			</div>
+			{/* <div className="col s12 white-text"><h5>spacer</h5></div> */}
 			
 			<div className="col s1">Image</div>
 			<div className="col s3">Name</div>
@@ -14,14 +61,16 @@ class EmployeeDirectory extends React.Component {
 			<div className="col s4">Email</div>
 			<div className="col s2">DOB</div>
 			
-			<EmployeeLine name="bob" cell="303-808-2312" email="bob@cool.dude" dob="4-20-1999" picture="nothing.jpg"/>
-			<EmployeeLine name="bob" cell="303-808-2312" email="bob@cool.dude" dob="4-20-1999" picture="nothing.jpg"/>
-			<EmployeeLine name="bob" cell="303-808-2312" email="bob@cool.dude" dob="4-20-1999" picture="nothing.jpg"/>
-			<EmployeeLine name="bob" cell="303-808-2312" email="bob@cool.dude" dob="4-20-1999" picture="nothing.jpg"/>
-			<EmployeeLine name="bob" cell="303-808-2312" email="bob@cool.dude" dob="4-20-1999" picture="nothing.jpg"/>
-			<EmployeeLine name="bob" cell="303-808-2312" email="bob@cool.dude" dob="4-20-1999" picture="nothing.jpg"/>
-			<EmployeeLine name="bob" cell="303-808-2312" email="bob@cool.dude" dob="4-20-1999" picture="nothing.jpg"/>
-			<EmployeeLine name="bob" cell="303-808-2312" email="bob@cool.dude" dob="4-20-1999" picture="nothing.jpg"/>
+			<div className="col s12 white-text"><h5>spacer</h5></div>
+			
+			{items.map(item => (
+				<EmployeeLine picture={item.picture}
+								name={item.name}
+								cell={item.cell}
+								email={item.email}
+								dob={item.dob}
+				/>
+			))}
 			
 		</div>;
 	}
--- 2020-08-02-React/app/src/EmployeeLine.js
+++ 2020-08-02-React/app/src/EmployeeLine.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 class EmployeeLine extends React.Component {
 	render() {
-		return <div>
-			<div className="col s1">{this.props.picture}</div>
+		return <div className="col s12 row">
+			<div className="col s1"><img src={this.props.picture}></img></div>
 			<div className="col s3">{this.props.name}</div>
 			<div className="col s2">{this.props.cell}</div>
 			<div className="col s4">{this.props.email}</div>
-			<div className="col s2">{this.props.dob}</div>
+			<div className="col s2">{this.props.dob.substring(0, 10)}</div>
 		</div>
 	}
 }
