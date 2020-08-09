2b36c8009ff78e424983bdbb23535990130a9b06
ninjapretzel <ninjapretzel@yahoo.com>
2020-08-09 16:28:43 -0600

    Adds sorting to header

--- 2020-08-02-React/app/src/EmployeeDirectory.js
+++ 2020-08-02-React/app/src/EmployeeDirectory.js
@@ -20,7 +20,7 @@ class EmployeeDirectory extends React.Component {
 				|| (employee.name.toLowerCase().includes(search))
 				|| (employee.email.toLowerCase().includes(search))
 				|| (employee.dob.toLowerCase().includes(search))
-				|| (employee.cell.toLowerCase().includes(search));
+				|| (employee.phone.toLowerCase().includes(search));
 			
 		}
 		
@@ -35,8 +35,8 @@ class EmployeeDirectory extends React.Component {
 				return {
 					name: it.name.first + " " + it.name.last,
 					email: it.email,
-					dob: it.dob.date,
-					cell: it.cell,
+					dob: it.dob.date.substring(0, 10),
+					phone: it.cell,
 					picture: it.picture.thumbnail,
 				}
 			});
@@ -59,14 +59,55 @@ class EmployeeDirectory extends React.Component {
 		this.setState({search: event.target.value});
 	}
 	
+	changeSortMode = (event) => {
+		const currentMode = this.state.sort;
+		const currentAsc = this.state.sortAsc;
+		
+		const clickedThing = event.target.innerText.toLowerCase();
+		
+		if (currentMode === clickedThing) {
+			if (!currentAsc) { // Clicked once, toggle sort direction
+				this.setState({sortAsc: true});
+			} else { // Clicked twice, revert to "default"/unsorted
+				this.setState({sort: "", sortAsc: false});
+			}
+			
+		} else { // Not clicked yet, sort by the clicked criteria 
+			this.setState({sort: clickedThing, sortAsc: false});
+		}
+		
+		
+		
+		
+	}
+	
 	render() {
-		const { error, isLoaded, items } = this.state;
+		const { error, isLoaded, items, sort, sortAsc } = this.state;
 		
 		if (error) {
 			return <div>Error: {error.message}</div>;
 		} else if (!isLoaded) {
 			return <div>Loading...</div>
 		}
+		
+		// make a copy of items, and filter/sort the copy as needed.
+		const sorted = [...items].filter(this.employeeMatchesSearch);
+		
+		if (sort !== "") {
+			sorted.sort( (a,b) => {
+				if (a.hasOwnProperty(sort) && b.hasOwnProperty(sort)) {
+					return a[sort].localeCompare(b[sort]);
+				}
+				
+				return 0;
+			} );
+			
+			if (sortAsc) {
+				sorted.reverse();
+			}
+		}
+		
+		// ▲ ▼
 		
 		return <div className="row">
 			<div className="col s4 white white-text">.</div>
@@ -80,23 +121,22 @@ class EmployeeDirectory extends React.Component {
 			<div className="card col s12 row tight strong">
 				<div className="col s12 white white-text">.</div>
 				
-				<div className="col s1">Image</div>
-				<div className="col s3">Name</div>
-				<div className="col s2">Phone</div>
-				<div className="col s4">Email</div>
-				<div className="col s2">DOB</div>
+				<div className="col s1 fakeLink">Image</div>
+				<div className="col s3"> {this.state.sort === "name" && (this.state.sortAsc ? "▲" : "▼") } <a href="#" onClick={this.changeSortMode}>Name</a>  </div>
+				<div className="col s2"> {this.state.sort === "phone" && (this.state.sortAsc ? "▲" : "▼") } <a href="#" onClick={this.changeSortMode}>Phone</a>  </div>
+				<div className="col s4"> {this.state.sort === "email" && (this.state.sortAsc ? "▲" : "▼") } <a href="#" onClick={this.changeSortMode}>Email</a>  </div>
+				<div className="col s2"> {this.state.sort === "dob" && (this.state.sortAsc ? "▲" : "▼") } <a href="#" onClick={this.changeSortMode}>DOB</a>  </div>
 				
 				<div className="col s12 white white-text">.</div>
 			</div>
 			
 			
-			{items
-				.filter(this.employeeMatchesSearch)
+			{sorted
 				.map( (item, index) => (
 				<EmployeeLine index={index}
 								picture={item.picture}
 								name={item.name}
-								cell={item.cell}
+								phone={item.phone}
 								email={item.email}
 								dob={item.dob}
 				/>
--- 2020-08-02-React/app/src/EmployeeLine.js
+++ 2020-08-02-React/app/src/EmployeeLine.js
@@ -8,9 +8,9 @@ class EmployeeLine extends React.Component {
 		return <div className={classes}>
 			<div className="col s1"><img src={this.props.picture}></img></div>
 			<div className="col s3">{this.props.name}</div>
-			<div className="col s2">{this.props.cell}</div>
+			<div className="col s2">{this.props.phone}</div>
 			<div className="col s4">{this.props.email}</div>
-			<div className="col s2">{this.props.dob.substring(0, 10)}</div>
+			<div className="col s2">{this.props.dob}</div>
 		</div>
 	}
 }
