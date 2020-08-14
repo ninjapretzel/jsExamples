2d757571dba09991b91b905a4c027ac442105e9f
ninjapretzel <ninjapretzel@gmail.com>
2020-08-14 15:47:24 -0600

    Adds required components and router

--- 2020-08-14-ReactPortfolio/app/src/App.js
+++ 2020-08-14-ReactPortfolio/app/src/App.js
@@ -2,12 +2,26 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-	return (
-		<div className="App">
-			
-		</div>
-	);
+import FourOhFour from "./pages/FourOhFour";
+import Home from "./pages/Home";
+
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+class App extends React.Component {
+	
+	
+	render() {
+		return (
+			<Router>
+				<div>
+					<Switch>
+						<Route exact path="/" component={Home} />
+						<Route component={FourOhFour} />
+					</Switch>
+				</div>
+			</Router>
+		);
+	}
 }
 
 export default App;
---
+++ 2020-08-14-ReactPortfolio/app/src/pages/FourOhFour.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class FourOhFour extends React.Component {
+	constructor(props) {
+		super(props);
+		
+	}
+	
+	render() {
+		return <div>
+			<h1>404 - Page not found</h1>
+			
+		</div>	
+	}
+	
+}
+
+export default FourOhFour;
