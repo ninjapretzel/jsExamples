ebf5ca84f37dd53b24c3b7396100b2cecf454b60
ninjapretzel <ninjapretzel@yahoo.com>
2020-08-26 17:08:33 -0600

    Adds CORS package

--- 2020-08-25-ExpressReactLogin/server/app.js
+++ 2020-08-25-ExpressReactLogin/server/app.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const cors = require('cors')
+
+const corsConfig = {
+	origin: 'http://localhost:3000', // This should come from a config file!
+}
 
 const app = express();
+app.use(cors(corsConfig));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
 
 const login = require("./routes/login");
 app.use(login);
