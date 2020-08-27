399afffe89c4aef06c63a4da9c32e85f2492960c
ninjapretzel <ninjapretzel@yahoo.com>
2020-08-27 16:25:59 -0600

    adds password change route to server

--- 2020-08-25-ExpressReactLogin/server/routes/login.js
+++ 2020-08-25-ExpressReactLogin/server/routes/login.js
@@ -5,6 +5,78 @@ const db = require("../db");
 
 const router = express.Router();
 
+// TODO: Get this value from configuration
+const SECRET = "thisIsNotAGoodSecret,ChangeMePlease"; 
+
+router.post("/changePassword", async function(req, res) {
+	const { oldPassword, newPassword, confirmPassword, token } = req.body;
+	
+	if (!oldPassword || !newPassword || !confirmPassword || !token) {
+		res.json({
+			success: false,
+			message: "Missing information"
+		});
+		return;
+	}
+	
+	let id = null;
+	let username = null;
+	try {
+		const unpacked = jwt.verify(token, SECRET);
+		id = unpacked.id;
+		username = unpacked.username;
+	} catch (err) {
+		res.json({
+			success: false,
+			message: "failed to unpack token",
+			err,
+		});
+		return;
+	}
+	
+	const check = await db.User.findOne( { id } );
+	if (!check) {
+		res.json({
+			success: false,
+			message: "User no longer exists"
+		});
+		return;
+	}
+	
+	const hash = check.hash;
+	if (!await argon2.verify(hash, oldPassword)) {
+		res.json({
+			success: false,
+			message: "Old password does not match current password"
+		});
+		return;
+	}
+	
+	if (newPassword !== confirmPassword) {
+		res.json({
+			success: false,
+			message: "new passwords do not match"
+		});
+		return;
+	}
+	
+	check.hash = await argon2.hash(newPassword);
+	
+	try {
+		await check.save();
+	} catch (err) {
+		res.json({
+			success: false,
+			message: "database error",
+			err
+		});
+		return;
+	}
+	
+	res.json({ success: true });
+	
+});
+
 router.post("/login", async function(req, res) {
 	
 	const { username, password } = req.body;
@@ -36,12 +108,14 @@ router.post("/login", async function(req, res) {
 	
 	const token = jwt.sign(
 		{ id: check.id, username: check.username },
-		"thisIsNotAGoodSecret,ChangeMePlease",
+		SECRET,
 		{ expiresIn: 60 * 60 } // 1 hour
 	);
 	
 	res.json({ success: true, token });
 });
+
+
 
 
 module.exports = router;
